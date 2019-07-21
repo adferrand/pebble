f2d0f60a5b595480369efee3357e98bfd2d3dfc4
Adrien Ferrand <adrien.ferrand@eff.org>
2019-07-21 23:24:57 +0200

    Add test code

---
+++ test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+)
+
+func main() {
+	resolver := &net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, _, _ string) (net.Conn, error) {
+			d := net.Dialer{}
+			return d.DialContext(ctx, "udp", "4.3.2.1:404")
+		},
+	}
+	net.DefaultResolver = resolver
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 30)
+	defer cancelfunc()
+	_, err := resolver.LookupTXT(ctx, "stupid.entry.net")
+
+	fmt.Printf("Error is: %s\n", err)
+
+	// Result on Linux
+	// Err is: lookup stupid.entry.net on 127.0.0.53:53: dial udp 4.3.2.1:404: i/o timeout
+}
